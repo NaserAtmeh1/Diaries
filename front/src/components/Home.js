865120f3ed3b44d4aa0f723c6c67ddae8f7a9b80
NaserAtmeh1 <112665610+NaserAtmeh1@users.noreply.github.com>
2022-10-14 18:57:00 -0700

    --

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import {DatabaseNetworkPoint} from '@icon-park/react';
 
 export default function Home() {
   return (
     <div className='container'>
-        <div className='left'>
-            <img src='' className='logo' /> 
-            <form className='form1'>
-                <input className='field' type="email" />
-                <input className='field' type="password" />
-            </form>
+      <div className='left'>
+       <div className='logo'>
+       <DatabaseNetworkPoint theme="outline" size="150" fill="#333"/>
+        <h1>WonderHit</h1>
+       </div>
+          <form className='form1'>
+            <input placeholder='Email' className='field' type="email" />
+            <input placeholder='Password' className='field' type="password" />
             <button className='submit'>Login</button>
-            <h3>You dont have an account ? <Link to="/register">Register</Link></h3>
-        </div>
-        <img className='right' src='https://images.unsplash.com/photo-1562577309-4932fdd64cd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' />
-
-  </div>
+            <h3 className='routing'>You don't have an account ? <Link className='rot' to="/register">Regiser</Link></h3>
+          </form>
+      </div>
+      <img className='right' src='https://images.unsplash.com/photo-1562577309-4932fdd64cd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' />
+    </div>
 
     )
 }
--- front/src/components/Register.js
+++ front/src/components/Register.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import {DatabaseNetworkPoint} from '@icon-park/react';
+import axios from "axios"
 
 export default function Register() {
+
   return (
     <div className='container'>
       <div className='left'>
-      <DatabaseNetworkPoint theme="outline" size="24" fill="#333"/>
+       <div className='logo'>
+       <DatabaseNetworkPoint theme="outline" size="150" fill="#333"/>
+        <h1>WonderHit</h1>
+       </div>
           <form className='form'>
-          <input className='field' type="text" />
-          <input className='field' type="email" />
-          <input className='field' type="password" />
-          <input className='field' name='age' type="number" />
-          <input className='field' name='gender' type="text" />
+          <input placeholder='Username' className='field' type="text" />
+          <input placeholder='Email' className='field' type="email" />
+          <input placeholder='Password' className='field' type="password" />
+          <input placeholder='Age' className='field' name='age' type="number" />
+          <input placeholder='Gender' className='field' name='gender' type="text" />
           <div className='profilePic'>
-            <input className='field' id='file' type="file" />
+            <div className='Photo'></div>
+            <input className='field2' id='file' type="file" />
             <label for = "file"  className='uploadPic' >+</label>
           </div>
           <button className='submit'>Register</button>
-          <h3 className='routing'>You already hava an account ? <Link to="/">Login</Link></h3>
+          <h3 className='routing'>You already have an account ? <Link className='rot' to="/">Login</Link></h3>
         </form>
       </div>
       <img className='right' src='https://images.unsplash.com/photo-1562577309-4932fdd64cd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' />
