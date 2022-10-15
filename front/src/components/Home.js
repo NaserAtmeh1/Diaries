9d9a4522b6c25d7cd8094e86d92e6a5164396da9
NaserAtmeh1 <112665610+NaserAtmeh1@users.noreply.github.com>
2022-10-14 19:17:35 -0700

    -

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
             <input placeholder='Email' className='field' type="email" />
             <input placeholder='Password' className='field' type="password" />
             <button className='submit'>Login</button>
-            <h3 className='routing'>You don't have an account ? <Link className='rot' to="/register">Regiser</Link></h3>
+            <h3 className='routing'>You don't have an account ? <Link className='rot' to="/register">Register</Link></h3>
           </form>
       </div>
       <img className='right' src='https://images.unsplash.com/photo-1562577309-4932fdd64cd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' />
