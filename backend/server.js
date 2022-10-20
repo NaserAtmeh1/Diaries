e0009986ce84cdd003f43284ad2553c56d0cb679
NaserAtmeh1 <112665610+NaserAtmeh1@users.noreply.github.com>
2022-10-19 18:36:34 -0700

    --

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,13 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended: true}));
 
 const auth = require("./routes/auth")
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+
 app.use("/",auth)
-  
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
 
 
 
